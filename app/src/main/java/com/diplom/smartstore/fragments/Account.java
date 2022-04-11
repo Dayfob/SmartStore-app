2901e85de7dc189acd54bb50cf460ae29ed3c450
Dayfob <semembaevalihan19@gmail.com>
2022-04-11 08:09:50 +0600

    add fragment profile

--- app/src/main/java/com/diplom/smartstore/fragments/Account.java
+++ app/src/main/java/com/diplom/smartstore/fragments/Account.java
@@ -19,7 +19,7 @@ public class Account extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        View view = inflater.inflate(R.layout.listview, container, false);
+        View view = inflater.inflate(R.layout.fragment_profile, container, false);
 
 //        // load products
 //        DB_Handler db_handler = new DB_Handler(getActivity());
